c5aa00c6fff202481df8011d823d710a02ebcab3
Jalaluddin Al-Mursyidy Fadhlurrahman <jalaluddin.almursyidy@ikonsultan.com>
2022-01-20 14:55:00 +0700

    initial commit from spring initializer

---
+++ src/main/java/com/government/ePemilu/EPemiluApplication.java
@@ -0,0 +1,13 @@
+package com.government.ePemilu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EPemiluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EPemiluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/government/ePemilu/EPemiluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.government.ePemilu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EPemiluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
